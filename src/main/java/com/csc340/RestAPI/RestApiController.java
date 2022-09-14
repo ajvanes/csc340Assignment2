3f11fb2cb0c1071d4108be22b68333556849dd9f
Andrew van Es <ajvanes@uncg.edu>
2022-09-14 11:23:13 -0400

    commit

---
+++ src/main/java/com/csc340/RestAPI/RestApiController.java
@@ -0,0 +1,34 @@
+package com.csc340.RestAPI;
+
+import org.json.JSONObject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * This method uses an API to obtain and show the users IP address
+ *
+ * @author Andrew Van Es
+ */
+@RestController
+public class RestApiController {
+
+    @GetMapping("/ip")
+    public Object getThing() {
+        String url = "https://api.ipify.org?format=json";
+        RestTemplate restTemplate = new RestTemplate();
+        Object jSonIP = restTemplate.getForObject(url, Object.class);
+
+        String ipAddress = restTemplate.getForObject(url, String.class);
+
+        JSONObject jo = new JSONObject(ipAddress);
+
+        System.out.println(jo.toString());
+        String quoteAuthor = jo.getString("ip");
+        System.out.println(quoteAuthor);
+
+        return jSonIP;
+    }
+
+}
